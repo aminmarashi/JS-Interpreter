879927880c67c3ff6e56598f45317a0367070767
Amin Marashi <me@amin.codes>
2024-09-17 00:00:00 +0000

    Add post-publish-test.sh

---
+++ post-publish-test.sh
@@ -0,0 +1,20 @@
+npm link
+mkdir test
+pushd test
+echo '{}' > package.json
+npm install js-interpreter
+cat > test.js <<EOF
+const Interpreter = require('js-interpreter');
+
+const initFunc = (interpreter, scope) => {
+  interpreter.setProperty(scope, 'console',
+    interpreter.nativeToPseudo({
+      log(...args) { console.log(...args); },
+    }));
+};
+(new Interpreter('console.log(\"Importing works!\")', initFunc)).run();
+EOF
+node test.js
+echo "console.log('CLI works!');" | ./node_modules/.bin/js-interpreter
+popd
+rm -rf test
